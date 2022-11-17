5204b03ebc487eecb8b54d8dd1a28a6ac6e55ca5
Sam Evans <sgevans21@gmail.com>
2022-11-17 20:40:53 +0000

    first edition of skills, buttons and formattinh + minor tweaks

---
+++ Resources/JS/Scripts/collapse-button.js
@@ -0,0 +1,17 @@
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+};
+
+
+//NOTE TO SELF THIS DOESN'T WORK (YET) FIX AND INSERT INTO HTML
